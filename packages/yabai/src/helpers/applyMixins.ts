cb19aa6f4f41bb6e1fc5fe31c51ba679f6e2922f
mk2 <mk2@mk2.red>
2020-01-07 22:25:45 +0900

    fixivab

--- packages/yabai/src/helpers/applyMixins.ts
+++ packages/yabai/src/helpers/applyMixins.ts
@@ -1,4 +1,6 @@
-export default function applyMixins<T extends Record<string, any>>(derivedCtor: T, baseCtors: any[]): T {
+import { Class } from 'type-fest';
+
+export default function applyMixins<T>(derivedCtor: Class<T>, baseCtors: Class<any>[]): Class<T> {
   baseCtors.forEach(baseCtor => {
     Object.getOwnPropertyNames(baseCtor.prototype).forEach(name => {
       Object.defineProperty(derivedCtor.prototype, name, Object.getOwnPropertyDescriptor(baseCtor.prototype, name)!);
