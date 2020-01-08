e548cba4c311144bf3816c56acd6134a3caacfba
mk2 <mk2@mk2.red>
2020-01-08 22:30:01 +0900

    ababa

---
+++ packages/yabai/README.md
@@ -0,0 +1,27 @@
+# yabai
+
+- Yet Another BoostNote Application Implementation
+
+## How to Use
+
+### install yabai via npm
+
+```fish
+npm i -g yabai
+```
+
+### set `BOOSTNOTE_DATA_DIRECTORY`
+
+Set the path of the directory which you have stored Boostnote data.
+
+Example:
+
+```fish
+set -x BOOSTNOTE_DATA_DIRECTORY ~/Documents/Boostnote # recommend it will be persisted in config.fish
+```
+
+### use yabai
+
+```fish
+yabai
+```
--- scripts/prepare-dist.js
+++ scripts/prepare-dist.js
@@ -8,29 +8,32 @@ const rimraf = require('rimraf');
 const rootDir = path.resolve(process.cwd());
 
 // run bundle
-process.chdir('./packages/yabai');
+process.chdir(path.resolve(rootDir, 'packages', 'yabai'));
 spawnSync('npm', ['run', 'bundle'], { stdio: 'inherit' });
 process.chdir(rootDir);
 
 // make package
-rimraf.sync(path.resolve(rootDir, 'packages/publish'));
-fs.mkdirSync(path.resolve(rootDir, 'packages/publish'));
+rimraf.sync(path.resolve(rootDir, 'packages', 'publish'));
+fs.mkdirSync(path.resolve(rootDir, 'packages', 'publish'));
 
 // copy need files
 fs.copyFileSync(
-  path.resolve(rootDir, 'packages/yabai/dist/yabai.js'),
-  path.resolve(rootDir, 'packages/publish/yabai.js'),
+  path.resolve(rootDir, 'packages', 'yabai', 'dist', 'yabai.js'),
+  path.resolve(rootDir, 'packages', 'publish', 'yabai.js'),
 );
 fs.copyFileSync(
-  path.resolve(rootDir, 'packages/yabai/package.json'),
-  path.resolve(rootDir, 'packages/publish/package.json'),
+  path.resolve(rootDir, 'packages', 'yabai', 'package.json'),
+  path.resolve(rootDir, 'packages', 'publish', 'package.json'),
 );
-fs.copyFileSync(path.resolve(rootDir, 'README.md'), path.resolve(rootDir, 'packages/publish/README.md'));
+fs.copyFileSync(
+  path.resolve(rootDir, 'packages', 'yabai', 'README.md'),
+  path.resolve(rootDir, 'packages', 'publish', 'README.md'),
+);
 
 // edit package.json
-const packageJson = JSON.parse(fs.readFileSync(path.resolve(rootDir, 'packages/publish/package.json')));
+const packageJson = JSON.parse(fs.readFileSync(path.resolve(rootDir, 'packages', 'publish', 'package.json')));
 delete packageJson.scripts;
 delete packageJson.dependencies;
 delete packageJson.devDependencies;
 delete packageJson.importSort;
-fs.writeFileSync(path.resolve(rootDir, 'packages/publish/package.json'), JSON.stringify(packageJson, null, 4));
+fs.writeFileSync(path.resolve(rootDir, 'packages', 'publish', 'package.json'), JSON.stringify(packageJson, null, 4));
