679404696a1e4c1317ae0835d027e423bfd78b90
h-asakura <haruka.asakura@esm.co.jp>
2020-01-09 18:40:52 +0900

    make disposable

--- packages/yabai/src/helpers/mobx/ReactableMixin.ts
+++ packages/yabai/src/helpers/mobx/ReactableMixin.ts
@@ -1,10 +1,18 @@
 export const kReactableMethodNames = Symbol('ReactableMethodNames');
+export const kShouldDispose = Symbol('ShouldDispose');
 
 export default class ReactableMixin {
   makeReactable() {
     const reactableMethodNames = (this as any)[kReactableMethodNames] as string[];
     for (const methodName of reactableMethodNames) {
       (this as any)[methodName]();
+    }
+  }
+
+  unmakeReactable() {
+    const reactableMethodNames = (this as any)[kReactableMethodNames] as string[];
+    for (const methodName of reactableMethodNames) {
+      (this as any)[methodName](kShouldDispose);
     }
   }
 }
--- packages/yabai/src/helpers/mobx/reactionMethod.ts
+++ packages/yabai/src/helpers/mobx/reactionMethod.ts
@@ -1,5 +1,5 @@
-import { kReactableMethodNames } from '@/helpers/mobx/ReactableMixin';
-import { IReactionPublic, reaction } from 'mobx';
+import { kReactableMethodNames, kShouldDispose } from '@/helpers/mobx/ReactableMixin';
+import { IReactionDisposer, IReactionPublic, reaction } from 'mobx';
 
 export default function reactionMethod<T>(expression: (r: IReactionPublic) => T): MethodDecorator {
   return (target: any, methodName, descriptor: TypedPropertyDescriptor<any>) => {
@@ -7,15 +7,27 @@ export default function reactionMethod<T>(expression: (r: IReactionPublic) => T)
     target[kReactableMethodNames].push(methodName);
 
     const effectFn = descriptor.value as Function;
+    let disposer: IReactionDisposer | undefined;
     return {
       configurable: true,
       get() {
         // eslint-disable-next-line @typescript-eslint/no-this-alias
         const self = this;
-        return (() => {
-          reaction(expression, v => {
-            effectFn.apply(self, [v]);
-          });
+        return ((shouldDispose?: any) => {
+          if (shouldDispose === kShouldDispose) {
+            disposer?.();
+            disposer = undefined;
+            return;
+          }
+
+          if (disposer) {
+            // 既にreactionの登録が終わっていた場合は、普通のメソッド呼び出しなので、オリジナルのメソッドを呼び出す
+            effectFn.apply(self);
+          } else {
+            disposer = reaction(expression, v => {
+              effectFn.apply(self, [v]);
+            });
+          }
         }) as any;
       },
     };
