1aed977341e2fbc43c59457869e03cc80926d791
h-asakura <haruka.asakura@esm.co.jp>
2020-01-14 19:12:50 +0900

    feat

--- packages/yabai/src/helpers/mobx/reactionMethod.spec.ts
+++ packages/yabai/src/helpers/mobx/reactionMethod.spec.ts
@@ -1,3 +1,26 @@
-describe('', () => {
-  test('');
+import applyMixins from '@/helpers/mixin/applyMixins';
+import * as mobx from 'mobx';
+
+import ReactableMixin from './ReactableMixin';
+import reactionMethod from './reactionMethod';
+
+const dummyMethod = jest.fn();
+const reactionMock = jest.spyOn(mobx, 'reaction');
+
+interface TargetClass extends ReactableMixin {}
+class TargetClass {
+  @reactionMethod(() => ({}))
+  dummyMethod(...args: any[]) {
+    dummyMethod(args);
+  }
+}
+
+applyMixins(TargetClass, [ReactableMixin]);
+
+describe('reactionMethod', () => {
+  it('必要なメソッドがミックスインされていること', () => {
+    const target = new TargetClass();
+    expect(target.makeReactable).toEqual(expect.any(Function));
+    expect(target.unmakeReactable).toEqual(expect.any(Function));
+  });
 });
